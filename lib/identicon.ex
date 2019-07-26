2bf5256b7d9c99eb06bc085b78f090f9f98ba745
AfonSir <afonsir17@gmail.com>
2019-07-25 23:35:29 -0300

    Adding function to mirror the first two elements of a list

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,6 +12,12 @@ defmodule Identicon do
     |> Enum.chunk_every(3)
   end
 
+  def mirror_row(row) do
+    [ first, second | _tail ] = row
+
+    row ++ [ second, first ]
+  end
+
   def pick_color(%Identicon.Image{ hex: [r, g, b | _tail ] } = image) do
     %Identicon.Image{ image | color: { r, g, b } }
   end
